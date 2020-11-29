9752aadfbe5e909a08cab7083d2e871c8e2658bf
jcmeunier77 <jc.meunier77@gmail.com>
2020-11-29 04:51:06 +0100

    including api interface

--- api/src/utils/c_file_to_pix.py
+++ api/src/utils/c_file_to_pix.py
@@ -2,50 +2,34 @@ import numpy as np
 import open3d as o3d
 import rasterio as rio
 from rasterio import mask
-import os
-import subprocess
 from shapely.geometry import Point
 import scipy.interpolate as sci
 import matplotlib.pyplot as plt
+import lidario
 
 # target = 199414.11489207728, 223588.55983083232
 # path_to_tif = "../data/DSM_vlaanderen/DHMVIIDSMRAS1m_k09/GeoTIFF/DHMVIIDSMRAS1m_k09.tif"
 crop_directory = "api/src/data/output/"
 crop_name = "result"
+translator = lidario.Translator("geotiff", "np")
+
+# Translate the tif file and get the pandas.Dataframe
 
 
-class TiffTo3D :
+class TiffTo3D:
 
     def __init__(self, xtarget, ytarget, path_to_tif):
         self.xtarget = xtarget
         self.ytarget = ytarget
         self.path_to_tif = path_to_tif
 
-    @staticmethod
-    def tif_to_xyz(my_path):
-        """
-        What/Where to load and save
-        Create dir for processed files
-        Find all tif
-        Convert tif to XYZ
-        """
-        global file_names
-        source_directory = my_path
-        target_directory = my_path
-        if not os.path.exists(target_directory):
-            os.makedirs(target_directory)
-        file_names = [files for files in os.listdir(source_directory) if files.endswith(".tif")]
-        for file_name in file_names:
-            gdal_command = "gdal_translate -of XYZ \"" + source_directory + "\\" + file_name + "\" \"" + target_directory + "\\" + file_name[:-3] + "xyz" + "\""
-            subprocess.call(gdal_command)
-
-    def target_to_buffer (self):
+    def target_to_buffer(self):
         """
         Creating a squared buffer of 40 pixels around target point
         """
         return Point(self.xtarget, self.ytarget).buffer(20, cap_style=3)
 
-    def tif_crop (self):
+    def tif_crop(self):
         """
         Crop tif file with rio.mask
         Increase its resolution with linear interpolation, RectBivariateSpline
@@ -57,21 +41,20 @@ class TiffTo3D :
         with rio.open(self.path_to_tif) as srcA:
             out_image, out_transform = rio.mask.mask(srcA, [self.target_to_buffer()], crop=True, filled=False)
             out_meta = srcA.meta
-        x_in, y_in,x_out,y_out=np.arange(0,out_image.shape[1],1), np.arange(0,out_image.shape[2],1),np.arange(0,out_image.shape[1],0.25),np.arange(0,out_image.shape[2],0.25)
+        x_in, y_in, x_out, y_out = np.arange(0, out_image.shape[1], 1), np.arange(0, out_image.shape[2], 1), \
+                                   np.arange(0, out_image.shape[1], 0.25), np.arange(0, out_image.shape[2], 0.25)
         f = sci.RectBivariateSpline(x_in, y_in, out_image.squeeze())
         outinter = f(x_out, y_out)
         outinter = outinter * 3
         outinter = np.where(outinter > np.percentile(outinter, 99.8), np.percentile(outinter, 99.8), outinter)
-        outinter=outinter.reshape(1,outinter.shape[0],outinter.shape[1])
+        outinter = outinter.reshape(1, outinter.shape[0], outinter.shape[1])
         outinter = outinter.astype('float32')
         out_meta.update({"driver": "GTiff", "height": outinter.shape[1], "width": outinter.shape[2], "transform": out_transform})
         with rio.open(f"{crop_directory}{crop_name}.tif", "w", **out_meta) as dest:
             dest.write(outinter)
 
     def crop_to_3D (self):
-        self.tif_to_xyz(crop_directory)
-        dataname=f"{crop_name}.xyz"
-        point_cloud= np.loadtxt(crop_directory+dataname,skiprows=1)
+        point_cloud = translator.translate(f"{crop_directory}{crop_name}.tif")
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(point_cloud[:,:3])
         pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
@@ -90,9 +73,7 @@ class TiffTo3D :
         return o3d.visualization.draw_geometries([density_mesh])
 
     def crop_to_3Dobj (self):
-        self.tif_to_xyz(crop_directory)
-        dataname=f"{crop_name}.xyz"
-        point_cloud= np.loadtxt(crop_directory+dataname,skiprows=1)
+        point_cloud = translator.translate(f"{crop_directory}{crop_name}.tif")
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(point_cloud[:,:3])
         pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
